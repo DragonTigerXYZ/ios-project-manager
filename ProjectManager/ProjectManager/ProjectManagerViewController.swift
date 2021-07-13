3935d0d626e7fac1bb9a530484a18db3c33836fd
DragonTigerXYZ <ybee1229@gmail.com>
2021-07-13 10:30:00 +0900

    [#2]feat: SceneDelegate를 통해 navigationController를 rootView로 지정

--- ProjectManager/ProjectManager/ProjectManagerViewController.swift
+++ ProjectManager/ProjectManager/ProjectManagerViewController.swift
@@ -6,13 +6,11 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ProjectManagerViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        view.backgroundColor = .white
     }
-
-
 }
 
