8d5d5b34a8f919699b780876ce99c84a285b4444
DragonTigerXYZ <ybee1229@gmail.com>
2021-07-13 11:44:46 +0900

    [#4]feat: 3개의 테이블뷰 생성

---
+++ ProjectManager/ProjectManager/DOINGTableViewController.swift
@@ -0,0 +1,31 @@
+//
+//  DOINGViewController.swift
+//  ProjectManager
+//
+//  Created by YB on 2021/07/13.
+//
+
+import UIKit
+
+class DOINGTableViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    override func numberOfSections(in tableView: UITableView) -> Int {
+
+        return 0
+    }
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+
+        return 0
+    }
+
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
+
+        return cell
+    }
+}
--- ProjectManager/ProjectManager/ProjectManagerViewController.swift
+++ ProjectManager/ProjectManager/ProjectManagerViewController.swift
@@ -8,14 +8,24 @@ import UIKit
 
 class ProjectManagerViewController: UIViewController {
 
+    let registerViewController = RegisterViewController()
+    let todoTableViewController = TODOTableViewController()
+    let doingTableViewController = DOINGTableViewController()
+    let doneTableViewController = DONETableViewController()
+    
+
     override func viewDidLoad() {
         super.viewDidLoad()
+        let rightBarButton = UIBarButtonItem.init(title: "+", style: .done, target: self, action: #selector(buttonPressed(_:)))
+        navigationItem.rightBarButtonItem = rightBarButton
+
         view.backgroundColor = .white
         navigationItem.title = "Project Manager"
 
-        let rightBarButton = UIBarButtonItem.init(title: "+", style: .done, target: self, action: nil)
-        navigationItem.rightBarButtonItem = rightBarButton
 
+        }
+    @objc func buttonPressed(_ sender: Any) {
+        self.present(registerViewController, animated: true, completion: nil)
     }
 }
 
