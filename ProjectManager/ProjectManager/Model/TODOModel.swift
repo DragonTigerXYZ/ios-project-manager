c4b93c18fdaad39ea638defb52ce9f90123ad420
DragonTigerXYZ <ybee1229@gmail.com>
2021-07-14 16:44:54 +0900

    [#9]feat: TODOModel 타입 구현

---
+++ ProjectManager/ProjectManager/Model/TODOModel.swift
@@ -0,0 +1,14 @@
+//
+//  TODOModel.swift
+//  ProjectManager
+//
+//  Created by YB on 2021/07/14.
+//
+
+import Foundation
+
+struct TODOModel {
+    let title: String
+    let deadline: Date
+    let description: String
+}
